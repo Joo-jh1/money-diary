27a9f3c923a703049a936041689dc51adbd75648
Joo-jh1 <newluckylady11@gmail.com>
2026-06-03 20:56:21 +0900

    Update sw (1).js

--- sw (1).js
+++ sw (1).js
@@ -1,6 +1,19 @@
-const CACHE = "yongdon-v3";
+const CACHE = "yongdon-v4";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/static/js/main.chunk.js",
+  "/static/js/bundle.js",
+  "/static/js/vendors~main.chunk.js",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json"
+];
 
 self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
   self.skipWaiting();
 });
 
@@ -12,13 +25,15 @@ self.addEventListener("activate", e => {
   );
 });
 
-// 항상 네트워크 우선 - 캐시 없이 최신 버전만
 self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  
   if (e.request.mode === "navigate") {
-    e.respondWith(fetch(e.request).catch(() => caches.match("/index.html")));
-  }
-});
-
-self.addEventListener("message", e => {
-  if (e.data === "skipWaiting") self.skipWaiting();
-});
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+          return res;
+        })
+        .catch(() =>
